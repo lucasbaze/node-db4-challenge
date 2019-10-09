8592653e0520566de87ebaf97250a161b95ab5f7
Lucas Bazemore <lucasabazemore@gmail.com>
2019-10-09 16:03:40 -0500

    set up express server

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const morgan = require('morgan');
+
+//
+//Custom Routes
+const recipeRoutes = require('./recipeRoutes/recipe-routes');
+
+//
+//Initialize server
+const server = express();
+
+//
+//setup middleware
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+server.use(morgan('tiny'));
+
+//
+//Inital get for testing
+server.get('/', (req, res) => {
+    res.send('Server is running!');
+});
+
+//
+//Route Handlers
+server.use('/api/recipes', recipeRoutes);
+
+//
+//Error handler
+server.use((err, req, res, next) => {
+    res.status(400).json(err);
+});
+
+//
+//Initialize PORT
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => `App is listening on PORT: ${PORT}`);
--- recipeRoutes/recipe-routes.js
+++ recipeRoutes/recipe-routes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const Recipes = require('./recipe-model');
 
-router.get('/', async (req, res) => {
-    let recipes = await Recipes.getRecipes();
-    res.status(200).json(recipes);
+router.get('/', (req, res) => {
+    res.send('Inside the route!');
+    // let recipes = await Recipes.getRecipes();
+    // res.status(200).json(recipes);
 });
 
 module.exports = router;
